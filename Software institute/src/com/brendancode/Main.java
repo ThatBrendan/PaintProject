9fbb71a7b22956ebdf219259abb39ff05f5e56f3
Brendan <macintosh@macintoshs-mbp.home>
2022-01-20 14:01:31 +0000

    made changes to price per gallon

---
+++ Software institute/src/com/brendancode/Main.java
@@ -0,0 +1,26 @@
+package com.brendancode;
+
+public class Main {
+    public static void main(String[] args) {
+
+       int paintGallonPerSquareFoot = 2;
+        int costPerPaintGallon = 22;
+        int paintPerRoom = 8;
+        int squareFoot = 400;
+        int roomsInHouse = 6;
+
+        int costPerRoom = costPerPaintGallon * paintPerRoom;
+        int costPerHouse = costPerRoom * roomsInHouse;
+
+        String quote = "It will cost you £" + costPerRoom + " to paint your room";
+        String quoteForHouse = "It will cost you approximately £" + costPerHouse + " to paint your house";
+
+        System.out.println(quote);
+        System.out.println(quoteForHouse);
+
+
+        int luckyNumber = (int) (Math.random() * 9);
+
+        System.out.println(luckyNumber);
+    }
+}
---
+++ learning-java/src/com/brendancode/Main.java
@@ -0,0 +1,15 @@
+package com.brendancode;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int number = 20;
+        String brand = "BrendanCode";
+
+        char nameInitial = 'A';
+        System.out.println(brand);
+        System.out.println(nameInitial);
+        System.out.println(number);
+    }
+}
