f2bb7de603b9fd73d078de1335a713b51268e3a0
ThatBrendan <ugobrendan@gmail.com>
2022-01-21 09:13:00 +0000

    refactored the swtich statement

--- Software institute/src/com/brendancode/Main.java
+++ Software institute/src/com/brendancode/Main.java
@@ -9,12 +9,23 @@ public class Main {
 
         int costPerPaintGallon = 22;
         int paintPerRoom = 6;
-        int roomsInHouse = 8;
         int budget = 2000;
         boolean weatherIsNice = false;
+        int roomsInHouse = 3;
 
         int costPerRoom = costPerPaintGallon * paintPerRoom;
         int costPerHouse = costPerRoom * roomsInHouse;
+
+        switch (roomsInHouse) {
+            case 0:
+                System.out.println("This is a caravan");
+                break;
+            case 1,2,3:
+                System.out.println("This is decent");
+                break;
+            default:
+                System.out.println("Let us chat about it");
+        };
 
         String quote = "It will cost you £" + costPerRoom + " to paint your room";
         String quoteForHouse = "It will cost you approximately £" + costPerHouse + " to paint your house";
@@ -29,13 +40,7 @@ public class Main {
         }
 
 
-        switch (roomsInHouse) {
-            case 0:
-                System.out.println("This is a caravan");
-                    break;
-            case 1,2,3:
-                System.out.println("This is decent");
-            break;
-        }
+
+
     }
 }
