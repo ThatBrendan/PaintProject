f3d23a2c7ffdae0d8b0932ed616231da499e93b0
ThatBrendan <ugobrendan@gmail.com>
2022-01-21 11:04:35 +0000

    Added loops for paint options

--- Software institute/src/com/brendancode/Main.java
+++ Software institute/src/com/brendancode/Main.java
@@ -3,26 +3,30 @@ package com.brendancode;
 public class Main {
     public static void main(String[] args) {
 
-        int luckyNumber = (int) (Math.random() * 9);
+        int weekOfTheMonth = (int) (Math.random() * 5);
 
-        System.out.println(luckyNumber);
+        System.out.println("You have booked to have your house painted on the " + weekOfTheMonth + " week of the month");
 
         int costPerPaintGallon = 22;
         int paintPerRoom = 6;
         int budget = 2000;
-        boolean weatherIsNice = true;
-        int roomsInHouse = 3;
+        boolean weatherIsNice = false;
+        int roomsInHouse = 6;
+
+        String [] typesOfPaint = {"Oil", "Acrylic", "Watercolor"};
+        for (String i : typesOfPaint) {
+            System.out.println(i + " might be a suitable choice of colour for your building");
+        }
 
         int costPerRoom = costPerPaintGallon * paintPerRoom;
         int costPerHouse = costPerRoom * roomsInHouse;
 
-        switch (luckyNumber) {
-            case 0 -> System.out.println("Damn son you ain't lucky");
-            case 1, 2, 3 -> System.out.println("You kinda lucky");
-            case 4, 5, 6, 7, 8 -> System.out.println("You luckyyyyy!!!");
-            default -> System.out.println("Let us chat about it");
+        switch (weekOfTheMonth) {
+            case 0 -> System.out.println("I'm not sure if I am busy");
+            case 1, 2 -> System.out.println("I'm booked out for the week");
+            case 3, 4 -> System.out.println("I might have a slot, let me check");
+            default -> System.out.println("Can you please check online");
         }
-        ;
 
         String quote = "It will cost you £" + costPerRoom + " to paint your room";
         String quoteForHouse = "It will cost you approximately £" + costPerHouse + " to paint your house";
@@ -30,10 +34,10 @@ public class Main {
         System.out.println(quote);
         System.out.println(quoteForHouse);
 
-        if (weatherIsNice) {
+        if (weatherIsNice && budget > costPerHouse && weekOfTheMonth > 2) {
             System.out.println("Lets get painting!!!");
         } else {
-            System.out.println("You'd need to think about it");
+            System.out.println("You'd need to get another quote");
         }
 
 
