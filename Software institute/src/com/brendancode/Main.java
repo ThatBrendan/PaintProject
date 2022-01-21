3c9c8f909d20d8d3604056c43421aa440c0cdd21
ThatBrendan <ugobrendan@gmail.com>
2022-01-21 09:21:58 +0000

    cleaned up switch and if statements

--- Software institute/src/com/brendancode/Main.java
+++ Software institute/src/com/brendancode/Main.java
@@ -10,22 +10,19 @@ public class Main {
         int costPerPaintGallon = 22;
         int paintPerRoom = 6;
         int budget = 2000;
-        boolean weatherIsNice = false;
+        boolean weatherIsNice = true;
         int roomsInHouse = 3;
 
         int costPerRoom = costPerPaintGallon * paintPerRoom;
         int costPerHouse = costPerRoom * roomsInHouse;
 
-        switch (roomsInHouse) {
-            case 0:
-                System.out.println("This is a caravan");
-                break;
-            case 1,2,3:
-                System.out.println("This is decent");
-                break;
-            default:
-                System.out.println("Let us chat about it");
-        };
+        switch (luckyNumber) {
+            case 0 -> System.out.println("Damn son you ain't lucky");
+            case 1, 2, 3 -> System.out.println("You kinda lucky");
+            case 4, 5, 6, 7, 8 -> System.out.println("You luckyyyyy!!!");
+            default -> System.out.println("Let us chat about it");
+        }
+        ;
 
         String quote = "It will cost you £" + costPerRoom + " to paint your room";
         String quoteForHouse = "It will cost you approximately £" + costPerHouse + " to paint your house";
@@ -33,7 +30,7 @@ public class Main {
         System.out.println(quote);
         System.out.println(quoteForHouse);
 
-        if (budget > costPerHouse && weatherIsNice) {
+        if (weatherIsNice) {
             System.out.println("Lets get painting!!!");
         } else {
             System.out.println("You'd need to think about it");
